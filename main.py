aefc6a06d1f087a1679057fa92f5058b274f0bb6
apple <masta2288@gmail.com>
2021-12-01 18:54:02 +0200

    pipipka

---
+++ main.py
@@ -0,0 +1,73 @@
+import math
+def dividers():
+    print("\033[96m{}\033[0m".format("--------------------------------"))
+    numb = int(input("What Number do you want: "))
+    print("\033[96m{}\033[0m".format("--------------------------------"))
+    print("Divisors:")
+    summen = []
+    for i in range(1, numb + 1):
+        if (numb % i == 0):
+            summen.append (i)
+            print(i)
+
+    print("\033[96m{}\033[0m".format("--------------------------------"))
+    a = sum(summen)
+    print ("All summa :",end = " ")
+    print (a)
+
+
+
+
+
+
+def reversrs():
+  print("\033[96m{}\033[0m".format("--------------------------------"))
+  Number1 = int(input("What is your first number: "))
+  Number2 = int(input("What is your second number: "))
+  print("\033[96m{}\033[0m".format("--------------------------------"))
+  summa = range(Number1, Number2 + 1)
+  Divisors = []
+  Free = []
+  answer = []
+  for g in summa:
+    Divisors.append(g)
+    print("\033[91m{}\033[0m".format("--------------------------------"))
+  print("Dis is yours range: ", Divisors)
+  print("\033[91m{}\033[0m".format("--------------------------------"))
+
+  for pain in Divisors:
+    for i in range(1, Divisors[len(Divisors) - 1] + 1):
+      g = 0
+      if pain % i == 0:
+        Free.append(i)
+      answer = []
+
+  for i in Free:
+    if i not in answer:
+      answer.append(i)
+
+    print("all integer divisors: ", sorted(answer))
+
+
+
+def main():
+
+    if __name__ == '__main__':
+        choice = input("\033[93m Good afternoon, you are on my code, here you can know all the divisors of a given number, healthy numbers and their sum.\n"
+                    "\033[97m Ievadi burtu:\n"
+                    "\033[94mFunction 1 - which you will know all the divisors and the sum of them \033[91mif you need this function write (d) \n"
+                    "\033[94mFunction 2 - the logic stays the same, only the number of numbers changes from one number to another.\033[91mif you need this function write (r)")
+        if choice.lower() == 'd':
+            dividers()
+        if choice.lower() == 'r':
+            reversrs()
+        else:
+            print("\033[91myou chose the wrong letter")
+            exit(0)
+
+while True:
+       main()
+       restart = input('do you want again?').lower()
+       if restart == "yes":
+           main()
+
